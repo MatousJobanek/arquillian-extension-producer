842d7591ccb44405413757b3e20e5e1ea31da413
Matous Jobanek <mjobanek@redhat.com>
2015-04-24 13:57:43 +0200

    refactoring

--- arquillian-extension-producer-test/src/test/java/arquillian/extension/producer/test/MyProducerTest.java
+++ arquillian-extension-producer-test/src/test/java/arquillian/extension/producer/test/MyProducerTest.java
@@ -25,29 +25,43 @@ public class MyProducerTest {
     @ToProvideSpecific
     private Resources providedSpecificImplementation;
 
+    /**
+     * Checks if all new instances of {@link Resources} have been injected info the fields
+     */
     @Test
     public void injectFieldsTest() {
-        Assert.assertNotNull(enrichedImplementation);
-        Assert.assertEquals("enriched in field", enrichedImplementation.getParameter());
 
+        // should be injected an instance for field from the EnricherProducer class
+        Assert.assertNotNull(enrichedImplementation);
+        Assert.assertEquals("enriched into field", enrichedImplementation.getParameter());
+
+        // should be injected an instance from the ProviderProducer class - the default one
         Assert.assertNotNull(providedImplementation);
         Assert.assertEquals("provided without any qualifier", providedImplementation.getParameter());
 
+        // should be injected an instance from the ProviderProducer class - the specific one
         Assert.assertNotNull(providedSpecificImplementation);
         Assert.assertEquals("provided with ToProvideSpecific qualifier", providedSpecificImplementation.getParameter());
     }
 
+    /**
+     * Checks if all new instances of {@link Resources} have been injected info the method parameters
+     */
     @Test
-    public void injectParamsTest(@ToEnrich Resources enrichedParam,
+    public void injectParamsTest(
+        @ToEnrich Resources enrichedParam,
         @ArquillianResource Resources providedParam,
         @ArquillianResource @ToProvideSpecific Resources providedSpecificParam) {
 
+        // should be injected an instance for method parameter from the EnricherProducer class
         Assert.assertNotNull(enrichedParam);
-        Assert.assertEquals("enriched in method", enrichedParam.getParameter());
+        Assert.assertEquals("enriched into method", enrichedParam.getParameter());
 
+        // should be injected an instance from the ProviderProducer class - the default one
         Assert.assertNotNull(providedParam);
         Assert.assertEquals("provided without any qualifier", providedParam.getParameter());
 
+        // should be injected an instance from the ProviderProducer class - the specific one
         Assert.assertNotNull(providedParam);
         Assert.assertEquals("provided with ToProvideSpecific qualifier", providedSpecificParam.getParameter());
     }
